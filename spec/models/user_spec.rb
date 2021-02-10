a3f0c92ac41d1173176bb75917ed15adceebd671
saiki0818 <kasakasa@gmail.com>
2021-02-10 16:58:32 +0900

    コントローラーのテストファイル作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -86,15 +86,4 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
   end
-
-  describe "トップページ" do
-    it "ログアウト状態では、ヘッダーに新規登録/ログインボタンが表示されること" do 
-    end
-    it "ログイン状態では、ヘッダーにユーザーのニックネーム/ログアウトボタンが表示されること" do       
-    end
-    it "ヘッダーの新規登録/ログインボタンをクリックすることで、各ページに遷移できること" do 
-    end
-    it "ヘッダーのログアウトボタンをクリックすることで、ログアウトができること" do 
-    end
-  end
 end
---
+++ spec/requests/products_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+RSpec.describe "ProductsController", type: :request do
+
+
+  describe 'トップページ' do
+    it "ログアウト状態では、ヘッダーに新規登録/ログインボタンが表示されること" do
+    end
+    it "ログイン状態では、ヘッダーにユーザーのニックネーム/ログアウトボタンが表示されること" do       
+    end
+    it "ヘッダーの新規登録/ログインボタンをクリックすることで、各ページに遷移できること" do 
+    end
+    it "ヘッダーのログアウトボタンをクリックすることで、ログアウトができること" do 
+    end
+  end
+end
