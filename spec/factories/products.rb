48d3946efb354a313a832b6fe546e55952c5004f
saiki0818 <kasakasa@gmail.com>
2021-02-11 19:41:18 +0900

    テストコード、factorybotの定義を修正

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :product do
     product_name      {'ゲーム'}
     description       {'アクションゲーム'}
-    category_id       {'6'}
-    state_id          {'3'}
-    deliver_fee_id    {'2'}
-    shipment_id       {'30'}
-    shipment_day_id   {'2'}
-    price             {'4500'}
+    category_id       {6}
+    state_id          {3}
+    deliver_fee_id    {2}
+    shipment_id       {30}
+    shipment_day_id   {2}
+    price             {4500}
     association :user
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -28,29 +28,29 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Description can't be blank")
      end
      it "カテゴリーの情報が必須であること" do
-      @product.category_id = nil
+      @product.category_id = 1
       @product.valid?
-      expect(@product.errors.full_messages).to include("Category can't be blank")
+      expect(@product.errors.full_messages).to include("Category must be other than 1")
      end
      it "商品の状態についての情報が必須であること" do
-      @product.state_id = nil
+      @product.state_id = 1
       @product.valid?
-      expect(@product.errors.full_messages).to include("State can't be blank")
+      expect(@product.errors.full_messages).to include("State must be other than 1")
      end
      it "配送料の負担についての情報が必須であること" do
-      @product.deliver_fee_id = nil
+      @product.deliver_fee_id = 1
       @product.valid?
-      expect(@product.errors.full_messages).to include("Deliver fee can't be blank")
+      expect(@product.errors.full_messages).to include("Deliver fee must be other than 1")
      end
      it "発送元の地域についての情報が必須であること" do
-      @product.shipment_id = nil
+      @product.shipment_id = 1
       @product.valid?
-      expect(@product.errors.full_messages).to include("Shipment can't be blank")
+      expect(@product.errors.full_messages).to include("Shipment must be other than 1")
      end
      it "発送までの日数についての情報が必須であること" do
-      @product.shipment_day_id = nil
+      @product.shipment_day_id = 1
       @product.valid?
-      expect(@product.errors.full_messages).to include("Shipment day can't be blank")
+      expect(@product.errors.full_messages).to include("Shipment day must be other than 1")
      end
      it "販売価格についての情報が必須であること" do
       @product.price = nil
@@ -58,7 +58,7 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Price can't be blank")
      end
      it "販売価格は、¥300~¥9,999,999の間のみ保存可能であること" do
-      @product.price = '50'
+      @product.price = 50
       @product.valid?
       expect(@product.errors.full_messages).to include("Price must be greater than 299")
      end
