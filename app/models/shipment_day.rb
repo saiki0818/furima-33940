e2d7f2909a2db6d37bdd914a5f2fe038fd1c6d03
saiki0818 <kasakasa@gmail.com>
2021-02-15 10:25:37 +0900

    詳細ページの実装、条件分岐による表示の変更

--- app/models/shipment_day.rb
+++ app/models/shipment_day.rb
@@ -1,9 +1,9 @@
 class ShipmentDay < ActiveHash::Base
   self.data = [
     { id: 1, name: '選択してください' },
-    { id: 2, name: '1〜2で発送' },
-    { id: 3, name: '2〜3で発送' },
-    { id: 4, name: '4〜7で発送' }
+    { id: 2, name: '1〜2日で発送' },
+    { id: 3, name: '2〜3日で発送' },
+    { id: 4, name: '4〜7日で発送' }
   ]
 
   include ActiveHash::Associations
